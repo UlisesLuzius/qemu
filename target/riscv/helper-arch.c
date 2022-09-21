a7e47b55254b1dfb45e3425f43400c5cb6add7ff
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2022-09-21 11:27:34 +0200

    typo: fix

--- target/riscv/helper-arch.c
+++ target/riscv/helper-arch.c
@@ -6,11 +6,11 @@
 #include "exec/helper-arch.h"
 
 bool HELPER(vcpu_is_userland)(CPUState *cpu) { 
-    return ENV(cpu)->priv != 0; 
+    return ENV(cpu)->priv == 0; 
 }
 
 uint16_t HELPER(vcpu_get_asid)(CPUState *cpu) { 
-    uint64_t asid_bits = ENV(cpu)->satp & SATP64_ASID;
-    uint64_t asid = asid_hi >> 48;
+    uint64_t asid_hi_bits = ENV(cpu)->satp & SATP64_ASID;
+    uint64_t asid = asid_hi_bits >> 48;
     return asid;
 }

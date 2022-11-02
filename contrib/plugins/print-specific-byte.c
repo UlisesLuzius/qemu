43d0d7ffdd37c2c4ec1a6b631f1fecea6131f737
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2022-11-02 09:19:47 +0100

    fix

--- contrib/plugins/print-specific-byte.c
+++ contrib/plugins/print-specific-byte.c
@@ -33,6 +33,20 @@ static void vcpu_insn_exec(unsigned int vcpu_index, void *encoded)
     qemu_plugin_outs(rep->str);
 }
 
+static void vcpu_tb_exec(unsigned int vcpu_index, void *encoded)
+{
+    if(vcpu_index != 1) {return;}
+    uint8_t *addr = (uint8_t *) ((uint64_t) encoded & ~(0xFL << 60));
+    uint64_t bytesize = (uint64_t) encoded >> 60;
+    g_autoptr(GString) rep = g_string_new("");
+    g_string_append_printf(rep, "%lu:", bytesize);
+    for(int byte = 0; byte < bytesize; byte++) {
+        g_string_append_printf(rep, " %02x", addr[byte]);
+    }
+    g_string_append_printf(rep, "\n");
+    qemu_plugin_outs(rep->str);
+}
+
 static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
 {
     size_t n_insns;
@@ -42,22 +56,24 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
     uint64_t basic_block_addr = (uint64_t) qemu_plugin_insn_haddr(insn);
     bool is_user = qemu_plugin_is_userland(insn);
     n_insns = qemu_plugin_tb_n_insns(tb);
-    bool print_block = false;
+    bool print_block = true;
     size_t tot_bytes = 0;
-    if (n_insns < 4) {
+    if (!is_user) {
         for (i = 0; i < n_insns; i++) {
             insn = qemu_plugin_tb_get_insn(tb, i);
             size_t bytesize = qemu_plugin_insn_size(insn);
             tot_bytes += bytesize;
-            if(!is_user && bytesize == 2) {
-                print_block = true;            }
         }
     }
 
     if(print_block) {
         uint64_t encoded = tot_bytes << 60 | basic_block_addr;
-        qemu_plugin_register_vcpu_insn_exec_cb(insn, vcpu_insn_exec,
-                                           QEMU_PLUGIN_CB_NO_REGS, (void *) encoded);
+        qemu_plugin_register_vcpu_tb_exec_cb(tb, vcpu_tb_exec,
+                                         QEMU_PLUGIN_CB_NO_REGS,
+                                         (void *) encoded);
+        //qemu_plugin_register_vcpu_insn_exec_cb(insn, vcpu_insn_exec,
+        //                                   QEMU_PLUGIN_CB_NO_REGS, 
+        //                                   (void *) encoded);
     }
 
 }

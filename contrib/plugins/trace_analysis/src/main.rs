274587b3d800a85542ab85bc48bad4070e6ae08b
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2022-11-08 20:09:35 +0100

    build

--- contrib/plugins/trace_analysis/src/main.rs
+++ contrib/plugins/trace_analysis/src/main.rs
@@ -211,7 +211,7 @@ fn main() -> Result<(), io::Error> {
             let d = cs.disasm_all(&t.insts_bytes, 0).unwrap();
 
             for i in d.iter() {
-                if curr_inst % 1000000 == 0 {
+                if curr_inst % 100000000 == 0 {
                     println!("Insts[{}]", curr_inst);
 
                     println!("Groups:");

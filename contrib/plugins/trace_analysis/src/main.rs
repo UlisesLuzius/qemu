383664d45cb8bd6439aeb00c2315d6312946f850
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2022-11-08 16:55:25 +0100

    fix

--- contrib/plugins/trace_analysis/src/main.rs
+++ contrib/plugins/trace_analysis/src/main.rs
@@ -255,7 +255,11 @@ fn main() -> Result<(), io::Error> {
                                     inst_loads += 1;
                                     inst_stores += 1;
                                 },
-                                _ => println!("Did not find what kind of memory operation: {:?}", op)
+                                _ => {
+                                    println!("Did not find what kind of memory operation: {:?}", detail);
+                                    println!("{}", i);
+                                }
+
                             }
                         },
                         X86OperandType::Reg(_) => {
